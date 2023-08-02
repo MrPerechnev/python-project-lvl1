75006612771d442a2348322ac501fed165b2d441
Andrey Perechnev <aperechnev95@mail.ru>
2023-08-02 14:56:45 +0300

    update4

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,6 +7,6 @@ task = 'Find the greatest common divisor of given numbers.'
 def main():
     number1 = randint(1, 100)
     number2 = randint(1, 100)
-    question = f'{number1}, {number2}'
+    question = f'{number1} {number2}'
     correct_answer = str(gcd(number1, number2))
     return question, correct_answer
