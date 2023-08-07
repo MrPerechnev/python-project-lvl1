d248b990af1a1212392681a386200bd33761f5cb
Andrey Perechnev <aperechnev95@mail.ru>
2023-08-07 22:25:57 +0300

    update

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -3,13 +3,20 @@ from random import randint
 task = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def even(number):
-    return 'yes' if number % 2 == 0 else 'no'
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
 
 
 def main():
     number_min = 1
     number_max = 100
     question = randint(number_min, number_max)
-    correct_answer = even(question)
+    correct_answer = is_even(question)
+    if is_even(question):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
     return question, correct_answer
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,17 +3,29 @@ from random import randint
 task = 'What number is missing in the progression?'
 
 
+def line_formation(first_number, step):
+    list = []
+    for _ in range(10):
+        first_number = first_number + step
+        list.append(str(first_number))
+    return list
+
+
+def generation(list, missing_number):
+    missing_number = randint(0, 9)
+    list = str(list[missing_number])
+    list[missing_number] = '..'
+    question = ' '.join(list)
+    return question
+
+
 def main():
     number_min = 1
     number_max = 100
     first_number = randint(number_min, number_max)
     step = randint(1, 10)
-    list = []
-    for _ in range(10):
-        first_number = first_number + step
-        list.append(str(first_number))
+    list = line_formation(first_number, step)
     missing_number = randint(0, 9)
     correct_answer = str(list[missing_number])
-    list[missing_number] = '..'
-    question = ' '.join(list)
+    question = generation(list, missing_number)
     return question, correct_answer
