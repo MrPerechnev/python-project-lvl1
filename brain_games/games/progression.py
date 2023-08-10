f7f2f0fdbaa07ca44d55a9c3d0006c2450db4522
Andrey Perechnev <aperechnev95@mail.ru>
2023-08-10 23:48:23 +0300

    add changes

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,7 +3,7 @@ from random import randint
 task = 'What number is missing in the progression?'
 
 
-def main():
+def get_progression():
     number_min = 1
     number_max = 100
     step = randint(1, 10)
@@ -12,7 +12,12 @@ def main():
     for _ in range(10):
         first_number = first_number + step
         list.append(str(first_number))
-    missing_number = randint(0, 9)
+    return list
+
+
+def main():
+    list = get_progression()
+    missing_number = randint(0, 9) 
     correct_answer = str(list[missing_number])
     list[missing_number] = '..'
     question = ' '.join(list)
