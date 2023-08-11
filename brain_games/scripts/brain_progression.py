ecc4811b18ed114a4996129a593a912f507a5039
Andrey Perechnev <aperechnev95@mail.ru>
2023-08-11 15:09:43 +0300

    update1

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.the_same_logic import start
+from brain_games.the_same_logic import run
 from brain_games.games import progression
 
 
 def main():
-    start(progression)
+    run(progression)
 
 
 if __name__ == '__main__':
