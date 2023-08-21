c737ed7f2c7228a29d0969cad0836c7af4d35068
Andrey Perechnev <aperechnev95@mail.ru>
2023-08-21 15:10:49 +0300

    update6

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -10,19 +10,19 @@ LENGTH = 10
 TASK = 'What number is missing in the progression?'
 
 
-def get_progression(first_number, length, step):
+def get_progression(initial_term, length, common_difference):
     list = []
     for _ in range(length):
-        first_number = first_number + step
-        list.append(str(first_number))
+        initial_term = initial_term + common_difference
+        list.append(str(initial_term))
     return list
 
 
 def generate():
     length = LENGTH
-    step = randint(STEP_MIN, STEP_MAX)
-    first_number = randint(NUMBER_MIN, NUMBER_MAX)
-    list = get_progression(first_number, length, step)
+    common_difference = randint(STEP_MIN, STEP_MAX)
+    initial_term = randint(NUMBER_MIN, NUMBER_MAX)
+    list = get_progression(initial_term, length, common_difference)
     missing_number = randint(NUMBER_START, NUMBER_END - 1)
     correct_answer = str(list[missing_number])
     list[missing_number] = '..'
